e5818ded8af9e98a4fd0e5d55fd20dedfa540ce8
dromerosalem <58950426+dromerosalem@users.noreply.github.com>
2019-12-28 23:13:57 +0000

    created game logic

---
+++ memory-game/JS/main.js
@@ -0,0 +1,10 @@
+console.log("Up and running");
+
+let cardOne = 'queen';
+let cadTwo = 'queen';
+let cardThree = 'king';
+let cardFour = 'king';
+
+console.log('User fliped' + cardOne);
+console.log('User fliped' + cardThree);
+
