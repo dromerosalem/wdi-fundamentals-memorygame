2498a48ecfb502fbf77eb6f8b865559748a90d25
dromerosalem <58950426+dromerosalem@users.noreply.github.com>
2020-01-04 13:58:12 +0000

    created a game logic

--- memory-game/JS/main.js
+++ memory-game/JS/main.js
@@ -1,10 +1,23 @@
-console.log("Up and running");
 
-let cardOne = 'queen';
-let cadTwo = 'queen';
-let cardThree = 'king';
-let cardFour = 'king';
+let cards = ['queen', 'queen', 'king', 'king'];
+let cardsInPlay = [];
+let cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped" + " " + cardOne);
 
-console.log('User fliped' + cardOne);
-console.log('User fliped' + cardThree);
+let cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped" + " " + cardTwo);
+
+
+if (cardsInPlay.length === 2){ 
+	
+	if (cardsInPlay[0] === cardsInPlay[1]){
+	alert('You found a match');
+}
+
+    else if (cardsInPlay[0] !== cardsInPlay[1]){
+	alert('Sorry, try again');
+}
+}
 
