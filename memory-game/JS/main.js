84852d9c82c0d0dcc12135b876dff7fd071a4bbe
dromerosalem <58950426+dromerosalem@users.noreply.github.com>
2020-01-06 01:44:24 +0000

    created game logic

--- memory-game/JS/main.js
+++ memory-game/JS/main.js
@@ -1,23 +1,28 @@
 
 let cards = ['queen', 'queen', 'king', 'king'];
 let cardsInPlay = [];
-let cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped" + " " + cardOne);
 
-let cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped" + " " + cardTwo);
-
-
-if (cardsInPlay.length === 2){ 
+function checkForMatch() {	
 	
 	if (cardsInPlay[0] === cardsInPlay[1]){
 	alert('You found a match');
 }
-
     else if (cardsInPlay[0] !== cardsInPlay[1]){
 	alert('Sorry, try again');
 }
 }
+
+function flipCard (cardID) {
+
+if (cardsInPlay.length === 2){
+checkForMatch();
+}
+
+console.log('User Flipped' + ' ' + cards[cardID]);
+cardsInPlay.push(cards[cardID]);
+}
+flipCard(0);
+flipCard(2);
+
+
 
